f8922e73ebb458d622afc28b5173de85b5aaa8dd
mRcfps <1402491442@qq.com>
2017-12-12 09:03:35 +0800

    [ADD] ch14.unsynch

---
+++ src/ch12/unsynch/UnsynchBankTest.java
@@ -0,0 +1,20 @@
+package ch12.unsynch;
+
+/**
+ * This program shows data corruption when multiple threads access a data structure.
+ * @version 1.0.0
+ * @author mRc
+ */
+public class UnsynchBankTest {
+    public static final int NACCOUNTS = 100;
+    public static final double INITIAL_BALANCE = 1000;
+
+    public static void main(String[] args) {
+        Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+        for (int i = 0; i < NACCOUNTS; i++) {
+            TransferRunnable r = new TransferRunnable(b, i, INITIAL_BALANCE);
+            Thread t = new Thread(r);
+            t.start();
+        }
+    }
+}
