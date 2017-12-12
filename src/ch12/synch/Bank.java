28ebc74aa27b35a7d3a1d939da25fae267e79472
mRcfps <1402491442@qq.com>
2017-12-12 11:13:25 +0800

    [ADD] ch14.synch and ch14.synch2

---
+++ src/ch12/synch/Bank.java
@@ -0,0 +1,71 @@
+package ch12.synch;
+
+import java.util.concurrent.locks.*;
+
+/**
+ * A bank with a number of bank accounts that uses locks for serializing access.
+ * @version 1.0.0
+ * @author mRc
+ */
+public class Bank {
+    private final double[] accounts;
+    private Lock bankLock;
+    private Condition sufficientFunds;
+
+    /**
+     * Constructs the bank.
+     * @param n the number of accounts
+     * @param initialBalance the initial balance for each account
+     */
+    public Bank(int n, double initialBalance) {
+        accounts = new double[n];
+        for (int i = 0; i < accounts.length; i++) {
+            accounts[i] = initialBalance;
+        }
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
+    }
+
+    public void transfer(int from, int to, double amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            while (accounts[from] < amount) {
+                sufficientFunds.await();
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2fn", getTotalBalance());
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    /**
+     * Gets the sum of all account balances.
+     * @return the total balance
+     */
+    private double getTotalBalance() {
+        bankLock.lock();
+        try {
+            double sum = 0;
+
+            for (double a : accounts) {
+                sum += a;
+            }
+
+            return sum;
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    /**
+     * Gets the number of accounts in the bank.
+     * @return the number of accounts
+     */
+    public int size() {
+        return accounts.length;
+    }
+}
