6dbe965864269b04cd04601e4d225fdb600c99ec
mRcfps <1402491442@qq.com>
2017-12-15 20:58:32 +0800

    [ADD] ch10 packages

---
+++ src/ch13/shuffle/ShuffleTest.java
@@ -0,0 +1,20 @@
+package ch13.shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This program demonstrates the random shuffle and sort algorithms.
+ */
+public class ShuffleTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 49; i++)
+            numbers.add(i);
+        Collections.shuffle(numbers);
+        List<Integer> winningCombination = numbers.subList(0, 6);
+        Collections.sort(winningCombination);
+        System.out.println(winningCombination);
+    }
+}
